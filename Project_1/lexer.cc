bc4e437ee19aa7d7bb07165a19c49013a9a18575
QLewis <qplewis@asu.edu>
2017-07-10 10:56:10 -0700

    removed a TODO statement in lexer.cc

--- Project_1/lexer.cc
+++ Project_1/lexer.cc
@@ -20,7 +20,7 @@ string reserved[] = { "END_OF_FILE",
     "EQUAL", "COLON", "COMMA", "SEMICOLON",
     "LBRAC", "RBRAC", "LPAREN", "RPAREN",
     "NOTEQUAL", "GREATER", "LESS", "LTEQ", "GTEQ",
-    "DOT", "NUM", "ID", "ERROR", "BASE08NUM", "BASE16NUM", "REALNUM" // TODO: Add labels for new token types here (as string)
+    "DOT", "NUM", "ID", "ERROR", "BASE08NUM", "BASE16NUM", "REALNUM" //DONE: Add labels for new token types here (as string)
 };
 
 #define KEYWORDS_COUNT 5
